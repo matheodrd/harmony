80d2c8342c6fdb7983881dc9128729a9653575cd
matheodrd <matheodurand14@gmail.com>
2026-03-01 00:10:42 +0100

    feat: add fastapi hello world

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,8 @@
-def main() -> None:
-    print("Hello from harmony!")
+from fastapi import FastAPI
+
+app = FastAPI(title="Harmony")
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root() -> dict[str, str]:
+    return {"message": "Hello from Harmony :P"}
