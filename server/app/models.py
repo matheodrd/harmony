700f79945218f89581dcc73ca0cdf06dd6aebc0b
matheodrd <matheodurand14@gmail.com>
2026-03-01 19:18:41 +0100

    feat: add Server ORM model

--- server/app/models.py
+++ server/app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -12,3 +12,18 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String)
+
+    owned_servers: Mapped[list[Server]] = relationship(
+        back_populates="owner",
+        lazy="selectin",  # FIXME: change to "raise" and implement pagination
+    )
+
+
+class Server(Base):
+    __tablename__ = "servers"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100))
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    owner: Mapped[User] = relationship(back_populates="owned_servers", lazy="joined")
