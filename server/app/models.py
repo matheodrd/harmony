907bd80e7ce26c477a0d10f0fb17d76c188127cf
matheodrd <matheodurand14@gmail.com>
2026-03-01 18:42:16 +0100

    feat: add User ORM model

---
+++ server/app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String)
