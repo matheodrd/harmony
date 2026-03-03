2d05db2b57a2b72d75934246b1b5789c4268bef2
matheodrd <matheodurand14@gmail.com>
2026-03-03 02:03:15 +0100

    feat: add db tables creation in app lifespan

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,27 @@
+from contextlib import asynccontextmanager
+from typing import TYPE_CHECKING
+
 from fastapi import FastAPI
 
-app = FastAPI(title="Harmony")
+from app.database import engine
+from app.models import Base
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None]:
+    # TODO: use proper DB migration system (alembic?)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await engine.dispose()
+
+
+app = FastAPI(title="Harmony", lifespan=lifespan)
 
 
 @app.get("/")
