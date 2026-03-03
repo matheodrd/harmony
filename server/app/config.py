fa94180914207d885ac423fbaa5a7a8f3ce17860
matheodrd <matheodurand14@gmail.com>
2026-03-03 01:33:02 +0100

    feat: add settings containing postgres DB vars

---
+++ server/app/config.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_ignore_empty=True,
+    )
+
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    @computed_field
+    @property
+    def database_url(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=f"/{self.POSTGRES_DB}",
+        )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()  # pyright: ignore[reportCallIssue]
