4e2705b984c00c16b8a6eaa3b1818215a700aebd
matheodrd <matheodurand14@gmail.com>
2026-03-08 17:53:33 +0100

    feat: add CurrentUserDep dependency to get currently authenticated user

--- server/app/dependencies.py
+++ server/app/dependencies.py
@@ -1,8 +1,38 @@
 from typing import Annotated
 
-from fastapi import Depends
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from server.app.models import User
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app import crud
 from app.database import get_db
+from app.security import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: DBSessionDep,
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        username = decode_access_token(token)
+    except jwt.PyJWTError:
+        raise credentials_exception from None
+
+    user = await crud.get_user_by_username(db, username)
+    if user is None:
+        raise credentials_exception
+    return user
+
 
 DBSessionDep = Annotated[AsyncSession, Depends(get_db)]
+CurrentUserDep = Annotated[User, Depends(get_current_user)]
